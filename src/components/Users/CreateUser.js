f1aaca6097e0f6935ecc8c516f638f14f83f5a2d
evgen <parom.evgen@mail.ru>
2023-06-11 12:05:17 +0300

    check value in form and clean input after success

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,23 +1,54 @@
 import Card from "../UI/Card";
 import styles from "./CreateUser.module.css";
 import Button from "../UI/Button";
+import { useState } from "react";
 
 const CreateUser = (props) => {
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+
   const createUserHandler = (event) => {
     event.preventDefault();
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+      return;
+    }
+    if (+inputAge < 1) {
+      return;
+    }
+
+    console.log(inputName, inputAge);
+
+    setInputName("");
+    setInputAge("");
+  };
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={createUserHandler}>
         <label>Name</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          onChange={nameChangeHandler}
+          value={inputName}
+        />
         <label>Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={inputAge}
+        />
 
-        <Button type="submit" onClick={createUserHandler}>
-          Add user
-        </Button>
+        <Button type="submit">Add user</Button>
       </form>
     </Card>
   );
